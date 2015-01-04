332d198017d7609d0c169a7cf6b0d0c0154f8499
Kevin McKernan <kevin@digsublime.com>
2015-01-03 20:16:56 -0700

    Successfully created a master shortcode with attributes and variables, can now use one shortcode to get a pie chart of any itemmeta

--- inc/w2s_charts.php
+++ inc/w2s_charts.php
@@ -8,16 +8,21 @@ function percent($num_amount, $num_total) {
 
 function itemmeta_query($value) {
 	global $wpdb;
-// Query string to check wp_woocommerce_order_itemmeta for a specified meta_key
-	$sql = "SELECT meta_value FROM wp_woocommerce_order_itemmeta WHERE meta_key IN ('".$value."')";
+	// Query string to check wp_woocommerce_order_itemmeta for a specified meta_key
 
-// Run the query via $wpdb
+	// Live Data
+	// $sql = "SELECT meta_value FROM wp_woocommerce_order_itemmeta WHERE meta_key IN ('".$value."')";
+	
+	// Test Data
+	$sql = "SELECT meta_value FROM wp_kevin_test_data WHERE meta_key IN ('".$value."')";
+
+	// Run the query via $wpdb
 	$query = $wpdb->get_results($sql, ARRAY_N);
 
-// Create empty array $counter, which will contain the count of meta values. This will help build the graph data.
+	// Create empty array $counter, which will contain the count of meta values. This will help build the graph data.
 	$counter = array();
 
-// Check each query result for a new value, and add them to $counter
+	// Check each query result for a new value, and add them to $counter
 	foreach ($query as $row) {
 		$meta_value =  $row[0];
 		if (!in_array($row, $counter)) {
@@ -25,17 +30,17 @@ function itemmeta_query($value) {
 		}
 	}
 
-/*
-At this point, the value of $counter is something like this:
+	/*
+	At this point, the value of $counter is something like this:
 
-Array
-(
-    [Tahosa] => 0
-    [Tupwee] => 0
-)
+	Array
+	(
+	    [Tahosa] => 0
+	    [Tupwee] => 0
+	)
 
-These nested foreach loops will run through $query, looking for matches between the $row and $key
-*/
+	These nested foreach loops will run through $query, looking for matches between the $row and $key
+	*/
 
 	foreach ($counter as $key => $value) {
 		foreach ($query as $row) {
@@ -58,122 +63,56 @@ function counter_value_return($key, $counter) {
 	return $value;
 }
 
-add_shortcode('query-function', 'w2s_query_function_test');
-function w2s_query_function_test() {
-	itemmeta_query('membership_level');
+function rand_color() {
+	$rand = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
+    $color = '#'.$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)];
+    return $color;
 }
 
-add_shortcode('lodge-breakdown', 'w2s_lodge_breakdown');
-
-function w2s_lodge_breakdown() {
-	ob_start();
-	wp_enqueue_script('chart-settings');
-	$count = itemmeta_query('lodge');
-
-?>
-<canvas id="lodgeBreakdown" width="400" height="400"></canvas>
-<script type="text/javascript">
-jQuery(document).ready( function() {
-
-// Chart Options
-var options = {
-<?php echo the_field('chart-options', 'option'); ?>
-}
-
-
-// Doughnut Chart Data
-var doughnutData = [
-	{
-		value : <?php echo counter_value_return('Tupwee', $count); ?>,
-		color : "purple",
-		label : "Tupwee <?php  ?>"
-	},
-	{
-		value : <?php echo counter_value_return('Ha-Kin-Skay-A-Ki', $count); ?>,
-		color : "#1789D4",
-		label : "Ha-Kin-Skay-A-Ki"
-	},
-	{
-		value : <?php echo counter_value_return('Tahosa', $count); ?>,
-		color : "#CB4B16",
-		label : "Tahosa"
-	},
-	{
-		value : <?php echo counter_value_return('Mic-O-Say', $count); ?>,
-		color : "#1F8261",
-		label : "Mic-O-Say"
-	},
-	{
-		value : <?php echo counter_value_return('Other', $count); ?>,
-		color : "#1F8261",
-		label : "Other"
-	}	
-
-]
-
-
-//Get the context of the Doughnut Chart canvas element we want to select
-var ctx = document.getElementById("lodgeBreakdown").getContext("2d");
-
-// Create the Doughnut Chart
-var mydoughnutChart = new Chart(ctx).Doughnut(doughnutData, options);
-});
-</script>
-
-<?php
-	$myvariable = ob_get_clean();
-	return $myvariable;
-}
-
-add_shortcode('chart-membership-level', 'w2s_membership_level');
-
-function w2s_membership_level() {
-	ob_start();
-	wp_enqueue_script('chart-settings');
-	$count = itemmeta_query('membership_level');
-
-?>
-<canvas id="membershipLevel" width="400" height="400"></canvas>
-<script type="text/javascript">
-jQuery(document).ready( function() {
-
-// Chart Options
-var options = {
-<?php echo the_field('chart-options', 'option'); ?>
-}
-
-
-// Doughnut Chart Data
-var doughnutData = [
-	{
-		value : <?php echo counter_value_return('Ordeal', $count); ?>,
-		color : "purple",
-		label : "Ordeal"
-	},
-	{
-		value : <?php echo counter_value_return('Brotherhood', $count); ?>,
-		color : "#1789D4",
-		label : "Brotherhood"
-	},
-	{
-		value : <?php echo counter_value_return('Vigil', $count); ?>,
-		color : "#CB4B16",
-		label : "Vigil"
-	},
-	{
-		value : <?php echo counter_value_return('Guest', $count); ?>,
-		color : "#1F8261",
-		label : "Guest"
+function w2s_donut_data($source) {
+	$final_json = array();
+	foreach ($source as $key => $value) {
+		$json = array();
+		$json['value'] = $value;
+		$json['color'] = rand_color();
+		$json['label'] = $key;
+		$final_json[] = $json;
 	}
+	echo json_encode($final_json);
+}
 
-]
+add_shortcode('chart-donut', 'w2s_donut_chart');
+
+function w2s_donut_chart( $atts ) {
+	ob_start();
+	extract( shortcode_atts( array (
+		'id' => '',
+		'query' => ''
+	), $atts ) );	
+	wp_enqueue_script('chart-settings');
+	$count = itemmeta_query($query);
+
+?>
+<canvas id="<?php echo $id; ?>" width="400" height="400"></canvas>
+<div id="<?php echo $id; ?>-legend"></div>
+<script type="text/javascript">
+jQuery(document).ready( function() {
+
+// Chart Options
+var options = {
+<?php echo the_field('chart-options', 'option'); ?>
+}
+
+// Doughnut Chart Data
+var doughnutData = <?php echo w2s_donut_data($count); ?>
 
 
 //Get the context of the Doughnut Chart canvas element we want to select
-var ctx = document.getElementById("membershipLevel").getContext("2d");
+var ctx = document.getElementById("<?php echo $id; ?>").getContext("2d");
 
 // Create the Doughnut Chart
-var mydoughnutChart = new Chart(ctx).Doughnut(doughnutData, options);
+var myDoughnutChart = new Chart(ctx).Doughnut(doughnutData, options);
+document.getElementById('<?php echo $id; ?>-legend').innerHTML = myDoughnutChart.generateLegend();
 });
 </script>
 
