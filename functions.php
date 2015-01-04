cd03487c1f274a439a4a261ee036ae2e4f560b12
Kevin McKernan <kevin@digsublime.com>
2015-01-03 19:00:33 -0700

    Shortcodes, functions, and scripts required for chart.js integration. At this point, the charts work. Making my code better now - going to make a single shortcode for all donut charts with options

--- functions.php
+++ functions.php
@@ -24,13 +24,15 @@ function remove_parent_theme_features() {
 
 // For simplicity's sake, create a file for each shortcode, and place it in the /inc directory. 
 // Then include it here, like this: require_once('inc/sublime_child_filterable_portfolio.php');
-
+require_once 'inc/w2s_charts.php';
 
 // Enqueue scripts & styles here
 function sublime_child_scripts() {
 	wp_enqueue_script( 'mix-it-up', '//cdn.jsdelivr.net/jquery.mixitup/latest/jquery.mixitup.min.js', array(), 'v1.5.6', true);	
 	wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/js/main.js', array(), '20120206', true );
 	wp_enqueue_style('dashicons');
+	wp_enqueue_script( 'chart-js', get_stylesheet_directory_uri().'/js/chart.js', true);
+	wp_register_script( 'chart-settings', get_stylesheet_directory_uri().'/js/chart-settings.js');
 }
 add_action( 'wp_enqueue_scripts', 'sublime_child_scripts' );
 
@@ -43,5 +45,16 @@ function sublime_load_adminjs() {
 
 add_action('admin_init', 'sublime_load_adminjs');
 
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Website Settings',
+		'menu_title'	=> 'Website Settings',
+		'menu_slug' 	=> 'website-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+};
 
+require_once 'inc/woocommerce-custom.php';
 ?>
---
+++ inc/woocommerce-custom.php
@@ -0,0 +1,8 @@
+<?php
+
+// apply_filters( 'wc_add_to_cart_message', 'w2s_conclave_add_to_cart_message');
+// function w2s_conclave_add_to_cart_message($message, $product_id) {
+//     return "Thank you for adding product" . $product_id;
+// }
+
+?>
